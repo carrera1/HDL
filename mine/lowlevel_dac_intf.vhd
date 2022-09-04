77c7d9defc6510334eb6275e6ba6c388c022b37e
Chris Carreras <carrera1@umbc.edu>
2022-09-04 18:19:55 -0400

    initial architecture added

--- mine/lowlevel_dac_intf.vhd
+++ mine/lowlevel_dac_intf.vhd
@@ -16,6 +16,15 @@ end lowlevel_dac_intfc;
 
 architecture behavioral of lowlevel_dac_intfc is
   -- signal definitations
+  signal reset_inv      : std_logic; -- active high reset
+  signal lrck_pulse     : std_logic;
+  signal lrck_internal  : std_logic;
+  signal bclk_pulse     : std_logic;
+  signal bclk_internal  : std_logic;
+  signal mclk_pulse     : std_logic;
+  signal mclk_internal  : std_logic;
+  signal bit_select     : natural range 0 to 31;
+
   -- Component declarations here
   component clkdivider is
     generic(divideby :natural:=2);
@@ -25,36 +34,92 @@ architecture behavioral of lowlevel_dac_intfc is
           pulseout : out std_logic
         );
   end component;
+
 begin
 
   -- logic goes here
-  -- Determine best way to instantiate module. Component vs Entity
-  -- TODO Instantiate clkdivider components for clock outputs
-  -- TODO Create & connect signals
+  reset_inv <= not s_axis_aresetn; -- Modules active high reset
+  lrck      <=     lrck_internal;
+  bclk      <=     bclk_internal;
+  mclk      <=     mclk_internal;
+
+  -- process to handle bit_select logic for sdata
+  bit_select_proc : process(s_axis_aclk, reset_inv)
+  begin
+    if reset_inv = '1' then
+      bit_select     <= 31;
+    elsif rising_edge(s_axis_aclk) then
+      if bclk_internal = '1' then
+        if bit_select = 0 then
+          bit_select <= 31;
+        else
+          bit_select <= bit_select - 1;
+      end if;
+    end if;
+  end process;
+
+  -- process to handle internal logic for module outputs
+  sdata_proc : process(s_axis_aclk, reset_inv, s_axis_tdata)
+  begin
+    if reset_inv = '1' then
+      sdata <= 0;
+    elsif rising_edge(s_axis_aclk) then
+      sdata <= s_axis_tdata(bit_select); -- TODO see if this works as a mux
+    end if;
+  end process;
+
+  lrck_proc : process(s_axis_aclk, reset_inv)
+  begin
+    if reset_inv = '1' then
+      lrck_internal <= 0;
+    elsif rising_edge(s_axis_aclk) then
+      if lrck_pulse = '1' then
+        lrck_internal <= not lrck_internal;
+      end if;
+  end process;
+
+  bclk_proc : process(s_axis_aclk, reset_inv)
+  begin
+    if reset_inv = '1' then
+      bclk_internal <= 0;
+    elsif rising_edge(s_axis_aclk) then
+      if bclk_pulse = '1' then
+        bclk_internal <= not bclk_internal;
+      end if;
+  end process;
+  
+  mclk_proc : process(s_axis_aclk, reset_inv)
+  begin
+    if reset_inv = '1' then
+      mclk_internal <= 0;
+    elsif rising_edge(s_axis_aclk) then
+      if mclk_pulse = '1' then
+        mclk_internal <= not mclk_internal;
+      end if;
+  end process;
 
   lrck_gen : clkdivider
   generic map(divideby => 2560)
   port map(
-            clk      => sig1,
-            reset    => sig2,
-            pulseout => sig3
+            clk     => s_axis_aclk,
+            reset   => reset_inv,
+            pulseout => lrck_pulse
           );
 
   bclk_gen : clkdivider
   generic map(divideby => 80)
   port map(
-            clk      => sig4,
-            reset    => sig5,
-            pulseout => sig6
+            clk     => s_axis_aclk,
+            reset   => reset_inv,
+            pulseout => bclk_pulse
           );
 
   mclk_gen : clkdivider
   generic map(divideby => 10)
   port map(
-            clk      => sig7,
-            reset    => sig8,
-            pulseout => sig9
+            clk     => s_axis_aclk,
+            reset   => reset_inv,
+            pulseout => mclk_pulse
           );
-
 
 end behavioral;
